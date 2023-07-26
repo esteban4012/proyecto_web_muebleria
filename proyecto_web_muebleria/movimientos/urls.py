aecec8c7e2a42d2b6af0de11ab681a7761edee60
esteban <esteban4012223@gmail.com>
2023-07-25 23:03:24 -0500

    segundo commit

--- proyecto_web_muebleria/movimientos/urls.py
+++ proyecto_web_muebleria/movimientos/urls.py
@@ -6,5 +6,8 @@ app_name = 'movimientos'
 
 urlpatterns = [
     path('home/', views.home, name='movimientos'),
-    path('home/crearMovimiento/', views.crearMovimiento, name='crear_movimiento')
+    path('home/crearMovimiento/', views.crearMovimiento, name='crear_movimiento'),
+    path('home/eliminarMovimiento/<id>/', views.eliminarMovimiento, name='eliminar_movimiento'),
+    path('home/editarMovimiento/<id>/', views.editarMovimiento, name='editar_movimiento'),
+    path('home/edicionMovimiento/', views.edicionMovimiento, name='edicion_movimiento')
 ]
--- proyecto_web_muebleria/movimientos/views.py
+++ proyecto_web_muebleria/movimientos/views.py
@@ -26,3 +26,32 @@ def crearMovimiento(request):
     movimiento = Movimiento.objects.create(cantidad = cantidad, valor_total = valor_total, id_orden = id_orden_obj, id_elemento = id_elemento_obj)
 
     return redirect('/movimientos/home')
+
+def editarMovimiento(request, id):
+    movimiento = Movimiento.objects.get(id = id)
+    elemento = Elementos.objects.all()
+    orden = Orden.objects.all()
+    return render(request, 'movimientos/editarMovimiento.html', {'movimiento': movimiento, 'elemento': elemento, 'orden': orden})
+
+def edicionMovimiento(request):
+    id = request.POST['numId']
+    cantidad = request.POST['numCantidad']
+    valor_total = request.POST['numValor']
+    id_orden = request.POST['idOrden']
+    id_elemento = request.POST['idElemento']
+
+    id_orden_obj = Orden.objects.get(pk=id_orden)
+    id_elemento_obj = Elementos.objects.get(pk=id_elemento)
+    movimiento = Movimiento.objects.get(id = id)
+    movimiento.cantidad = cantidad
+    movimiento.valor_total = valor_total
+    movimiento.id_orden = id_orden_obj
+    movimiento.id_elemento = id_elemento_obj
+    movimiento.save()
+    return redirect('/movimientos/home')
+
+
+def eliminarMovimiento(request, id):
+    movimiento = Movimiento.objects.get(id = id)
+    movimiento.delete()
+    return redirect('/movimientos/home')
