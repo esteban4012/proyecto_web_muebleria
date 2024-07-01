249b7a300713e3dbbd9c5eeb5304381757a68de0
esteban <esteban4012223@gmail.com>
2024-06-30 21:47:15 -0500

    agregando archivo requetiments.txt

--- proyecto_web_muebleria/proyecto_web_muebleria/settings.py
+++ proyecto_web_muebleria/proyecto_web_muebleria/settings.py
@@ -82,16 +82,15 @@ WSGI_APPLICATION = 'proyecto_web_muebleria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inventario',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }   
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 
 # Password validation
